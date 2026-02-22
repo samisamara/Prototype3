0590d738f6ea0991cd5e62f2f8065e79897d7631
Sami Samara <77996431+samisamara@users.noreply.github.com>
2026-02-21 23:16:37 -0600

    Completed Jumping mechanic, set parameter to check if player is on ground or not to limit infinite jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float jumpPower = 10;
+    [SerializeField] private float gravityModifier = 1;
     private Rigidbody rb;
+    private bool isOnGround = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity *=  gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log(rb.transform.position);
     }
 
     public void Jump(InputAction.CallbackContext input)
     {
-        Debug.Log("Jump!");
+        if (input.performed && isOnGround)
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
